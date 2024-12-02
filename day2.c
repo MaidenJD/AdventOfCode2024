765c1c8059d19c87e09817af41fcc021ea805dc9
James Maiden <i.am@jamesmaiden.co.uk>
2024-12-02 22:05:15 +0000

    Day 2 part 2

--- day2.c
+++ day2.c
@@ -4,6 +4,36 @@
 #define STB_DS_IMPLEMENTATION
 #include "stb/stb_ds.h"
 
+bool is_report_safe(int *report, size_t report_length) {
+    if (report_length >= 2) {
+        int expected_direction = report[1] - report[0];
+        expected_direction = expected_direction / abs(expected_direction);
+        if (expected_direction == 0) return false; // Unsafe report, duplicate levels!
+
+        for (size_t j = 1; j < report_length; j++) {
+            int delta = report[j] - report[j - 1];
+            if (delta == 0) {
+                //  Delta is 0, unsafe due to duplicate levels
+                return false;
+            }
+
+            int size = abs(delta);
+            if (size > 3) {
+                //  Delta is too large
+                return false;
+            }
+
+            int direction = delta / size;
+            if (direction != expected_direction) {
+                //  Direction has changed
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
 int main(int argc, char **argv) {
     FILE *input_file = fopen("input/day2.txt", "rb");
     if (input_file) {
@@ -13,6 +43,11 @@ int main(int argc, char **argv) {
             fseek(input_file, 0, SEEK_END);
             size_t input_length = ftell(input_file);
             rewind(input_file);
+
+            if (input_length == 0) {
+                fprintf(stderr, "Failed to read input\n");
+                return 1;
+            }
 
             char *input = malloc(input_length);
             size_t read = fread(input, sizeof(input[0]), input_length, input_file);
@@ -44,43 +79,33 @@ int main(int argc, char **argv) {
             free(input);
         }
 
-        size_t result11 = 0;
-        for (size_t i = 0; i < arrlenu(reports); i++) {
-            int *report = reports[i];
+        int scratch_buffer[1024] = { 0 };
 
-            if (arrlenu(report) >= 2) {
-                int expected_direction = report[1] - report[0];
-                expected_direction = expected_direction / abs(expected_direction);
-                if (expected_direction == 0) continue; // Unsafe report, duplicate levels!
+        size_t result1 = 0;
+        size_t result2 = 0;
+        for (size_t report_index = 0; report_index < arrlenu(reports); report_index++) {
+            int *report = reports[report_index];
 
-                bool safe = true;
-                for (size_t j = 1; j < arrlenu(report); j++) {
-                    int delta = report[j] - report[j - 1];
-                    if (delta == 0) {
-                        //  Delta is 0, unsafe due to duplicate levels
-                        safe = false;
-                        break;
+            bool safe_at_first_glance = is_report_safe(report, arrlenu(report));
+            if (safe_at_first_glance) {
+                result1 += 1;
+                result2 += 1;
+            } else {
+                for (size_t skip_index = 0; skip_index < arrlenu(report); skip_index++) {
+                    size_t scratch_index = 0;
+                    for (size_t i = 0; i < skip_index; i++, scratch_index++) {
+                        scratch_buffer[scratch_index] = report[i];
                     }
 
-                    int size = abs(delta);
-                    if (size > 3) {
-                        //  Delta is too large
-                        safe = false;
-                        break;
+                    for (size_t i = skip_index + 1; i < arrlenu(report); i++, scratch_index++) {
+                        scratch_buffer[scratch_index] = report[i];
                     }
 
-                    int direction = delta / size;
-                    if (direction != expected_direction) {
-                        //  Direction has changed
-                        safe = false;
+                    if (is_report_safe(scratch_buffer, arrlenu(report) - 1)) {
+                        result2 += 1;
                         break;
                     }
                 }
-
-                if (safe) result11 += 1;
-            } else {
-                // Single item report, automatically safe!
-                result11 += 1;
             }
         }
 
@@ -90,8 +115,8 @@ int main(int argc, char **argv) {
 
         arrfree(reports);
 
-        fprintf(stdout, "Day 2-1: %lu\n", result11);
-        // fprintf(stdout, "Day 2-2: %lu\n", result12);
+        fprintf(stdout, "Day 2-1: %lu\n", result1);
+        fprintf(stdout, "Day 2-2: %lu\n", result2);
     }
 
     return 0;
